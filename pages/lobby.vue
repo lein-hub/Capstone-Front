91ec364777a8e856e27285b4a10ed917c0240590
lein-hub <75424893+lein-hub@users.noreply.github.com>
2022-02-09 01:24:48 +0900

    start

---
+++ pages/lobby.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div class="gameBox">
+    <profile></profile>
+  </div>
+</template>
+<script>
+import profile from "@/components/profile.vue";
+
+export default {
+  components: {
+    profile,
+  },
+};
+</script>
+<style lang="scss">
+@import "~assets/game.scss";
+</style>
